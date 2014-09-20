d0d7920f39052aa2a5d55939bb47342677220a1d
thingywhat <thingywhat@live.com>
2014-09-20 04:11:02 -0600

    Made script more terse.

--- hornyPharohDownloader.js
+++ hornyPharohDownloader.js
@@ -6,8 +6,6 @@
 // @include       http://www.hornypharaoh.com/videos/*/*
 // ==/UserScript==
 
-document.querySelectorAll("#video_option td")[2]
-  .appendChild($('<button />').click(function(){
-    location.replace(JSON.parse(
-      $('[name=flashvars]').val().split("config=")[1]).clip.url);
-  }).text("Download!")[0]);
+$("#video_option td")[2].appendChild($('<button />').click(function(){
+  location.replace(JSON.parse($('[name=flashvars]').val().split("config=")[1]).clip.url);
+}).text("Download!")[0]);

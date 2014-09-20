4c63470a2bff521daa497e6eb463934707634ff1
thingywhat <thingywhat@live.com>
2014-09-20 04:36:38 -0600

    Added GreaseMonkey metadata.

--- hornyPharohDownloader.js
+++ hornyPharohDownloader.js
@@ -1,9 +1,12 @@
 // ==UserScript==
 // @name          Horny Pharoh Video downloader
+// @namespace     Horny Pharoh Video downloader
 // @namespace     hornyPharohDownloader.js
 // @description	  Allows the download of videos from hornypharoh.com
 // @author        thingywhat
 // @include       http://www.hornypharaoh.com/videos/*/*
+// @version       1
+// @grant         none
 // ==/UserScript==
 
 $("#video_option td")[2].appendChild($('<button />').click(function(){
